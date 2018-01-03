72d59a58aca7bf4833f968361f465c6fa38e38cb
jd.macmurtrie <jamesd.macmurtrie@gmail.com>
2018-01-03 10:35:44 -0500

    remove redux

--- containers/App.js
+++ containers/App.js
@@ -1,36 +1,84 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { bloat, shrink } from '../actions/sizeAdjust';
+// import { connect } from 'react-redux';
+// import { bloat, shrink } from '../actions/sizeAdjust';
 
-const App = props => {
-  let handleSizeChange = () => {
-    if(props.size == 'normal') {
-      props.bloat("bloated")
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      normal: true,
+      large: false
+    };
+    this.handleSizeChange = this.handleSizeChange.bind(this)
+    this.bloat = this.bloat.bind(this)
+    this.shrink = this.shrink.bind(this)
+  }
+
+  handleSizeChange() {
+    if(this.state.normal) {
+      this.bloat()
     } else {
-      props.shrink("normal")
+      this.shrink()
     }
   }
 
-  return (
-    <div className="goodStuff" onClick={handleSizeChange}>
-      <div className={props.size}>
-        Click Me!
+  bloat() {
+    this.setState({ normal: false, large: true })
+  }
+
+  shrink() {
+    this.setState({ normal: true, large: false })
+  }
+  render() {
+    let size
+    if (this.state.normal) {
+      size = "normal"
+    } else {
+      size = "bloated"
+    }
+    
+    return (
+      <div className="goodStuff" onClick={this.handleSizeChange}>
+        <div className={size}>
+          Click Me!
+        </div>
       </div>
-    </div>
-  );
-}
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    bloat: () => dispatch(bloat()),
-    shrink: () => dispatch(shrink())
+    );
   }
 }
 
-const mapStateToProps = (state) => {
-  return {
-    size: state.size
-  }
-}
+export default App
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+
+
+
+
+
+
+
+
+
+// const App = props => {
+//   let handleSizeChange = () => {
+//     if(props.size == 'normal') {
+//       props.bloat("bloated")
+//     } else {
+//       props.shrink("normal")
+//     }
+//   }
+
+//
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     bloat: () => dispatch(bloat()),
+//     shrink: () => dispatch(shrink())
+//   }
+// }
+//
+// const mapStateToProps = (state) => {
+//   return {
+//     size: state.size
+//   }
+// }
+//
+// export default connect(mapStateToProps, mapDispatchToProps)(App);
